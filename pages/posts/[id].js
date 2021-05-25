6f5a2c4dabb6fbec64f62bc908746713b86b9385
Isaac Sokari <isaacsokari@gmail.com>
2021-05-25 13:59:47 +0100

    feat: add page title and set html content for posts

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,21 @@
+import Head from 'next/head'
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+
       {postData.title}
       <br />
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   )
 }
@@ -22,7 +29,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
